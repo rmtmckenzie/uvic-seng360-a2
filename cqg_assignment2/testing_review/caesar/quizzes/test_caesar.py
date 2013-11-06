2416d5d4fb594922bfefe26750a89fdeb5429029
Morgan McKenzie <rmtm@uvic.ca>
2013-11-05 18:53:04 -0800

    did that do it?

---
+++ cqg_assignment2/testing_review/caesar/quizzes/test_caesar.py
@@ -0,0 +1,10 @@
+#A2 DONE
+question_list = [
+#	(mark, 	count, [directories])
+	(1,	1,	['test_caesar/short0']),
+	(1, 1,  ['test_caesar/long0'])
+]
+practice_mode = True
+standalone = False
+logged = False
+log_dir = ''
---
+++ cqg_assignment2/testing_review/caesar/templates/template.py
@@ -0,0 +1,10 @@
+group_list = [
+	['short',
+			['ABCDEFG',4,[0,4]],
+			['CNC',25,[1]]
+	],
+	['long',
+			['QWEAASDFASFQWBYYQWER',13,[0,3,5,7,9,13,16]],
+			['UTERHGFJSFDAERAFASDG',15,[1,4,6,8,10,14,17]]
+	]
+]
